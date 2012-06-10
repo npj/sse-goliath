ea8ed457fb7858af46af16801d09af24554d7901
Peter Brindisi <peter.brindisi@gmail.com>
2012-06-10 12:58:12 +0200

    default connect to local redis when no REDIS_URI in env

--- utils.rb
+++ utils.rb
@@ -1,6 +1,10 @@
 module Utils
   def self.redis_connect(options)
-    uri = URI.parse(ENV['REDIS_URI'])
-    Redis.new(options.merge(:host => uri.host, :port => uri.port, :password => uri.password))
+    if ENV['REDIS_URI']
+      uri = URI.parse()
+      Redis.new(options.merge(:host => uri.host, :port => uri.port, :password => uri.password))
+    else
+      Redis.new(options)
+    end
   end
 end
